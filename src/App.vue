eac5f6698e1e9b6f7be537190cccc4a48d2fee95
SmarkSeven <wuziq@vip.qq.com>
2017-04-19 22:08:35 +0800

    添加浏览器代理判断

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-   <transition name="one-fade-top">
-     <music-player class="player" v-show="showMusicPlayer"></music-player>
-   </transition>
-   <transition name="one-fade-right">
-      <player-toggle v-show="showPlayerToggle"></player-toggle>
-   </transition>
-    <transition
-      :name="direction === 'forward' ? 'one-pop-in' : 'one-pop-out'"
-    >
+    <transition name="one-fade-top">
+      <music-player class="player" v-show="showMusicPlayer && isMobile"></music-player>
+    </transition>
+    <transition name="one-fade-right">
+        <player-toggle v-show="showPlayerToggle && isMobile"></player-toggle>
+    </transition>
+    <transition v-if="isMobile" :name="direction === 'forward' ? 'one-pop-in' : 'one-pop-out'">
       <router-view class="router-view"></router-view>
     </transition>
+    <notification
+      :options="options"
+      :show="showNotification"
+      @close="closeNotification">
+    </notification>
   </div>
 </template>
 
@@ -18,17 +21,30 @@
 import { mapState } from 'vuex';
 import MusicPlayer from './components/MusicPlayer';
 import PlayerToggle from './components/PlayerToggle';
+import Notification from './components/Notification';
 
 export default {
   name: 'app',
   data() {
     return {
+      isMobile: true,
+      showNotification: false,
       showPlayerToggle: false,
+      options: {
+        autoClose: true,
+        backgroundColor: '#fc5050',
+        barColor: '#415f77',
+        countdownBar: false,
+        content: '这是一个 Mobile web 项目, 请用移动浏览器浏览',
+        showTime: 5000,
+        textColor: '#fff',
+      },
     };
   },
   components: {
     MusicPlayer,
     PlayerToggle,
+    Notification,
   },
   computed: {
     ...mapState({
@@ -43,12 +59,37 @@ export default {
       return 'animated fadeInLeft';
     },
   },
+  methods: {
+    closeNotification() {
+      this.showNotification = false;
+      this.options = {};
+    },
+    checkBrowser() {
+      const system = {};
+      const userAgent = navigator.userAgent;
+      system.iphone = userAgent.indexOf('iphone') !== -1;
+      system.android = userAgent.indexOf('Android') !== -1;
+      system.iPad = userAgent.indexOf('iPad') !== -1;
+      if (!system.iphone && !system.android && !system.iPad) {
+        this.isMobile = false;
+        this.showNotification = true;
+      } else {
+        this.isMobile = true;
+        this.showNotification = false;
+      }
+    },
+  },
   watch: {
     playState() {
       if (this.playState === 'playing' && !this.showPlayerToggle) {
         this.showPlayerToggle = true;
       }
     },
+  },
+  created() {
+    this.checkBrowser();
+    const body = document.querySelector('body');
+    body.onresize = this.checkBrowser;
   },
 };
 </script>
